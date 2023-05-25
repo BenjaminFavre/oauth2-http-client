28ecfcfbc808453ef0604206f9e2cae0829cd653
Roland Franssen :) <franssen.roland@gmail.com>
2023-05-25 14:36:53 +0200

    Add OAuth scope support

--- src/GrantType/ClientCredentialsGrantType.php
+++ src/GrantType/ClientCredentialsGrantType.php
@@ -23,21 +23,29 @@ class ClientCredentialsGrantType implements GrantTypeInterface
     private string $clientSecret;
 
     /**
+     * @var string[]
+     */
+    private array $scopes;
+    
+    /**
      * @param HttpClientInterface $client A HTTP client to be used to communicate with the OAuth server.
      * @param string $tokenUrl The full URL of the token endpoint of the OAuth server.
      * @param string $clientId The OAuth client ID.
      * @param string $clientSecret The OAuth client secret.
+     * @param string[] $scopes The OAuth scopes.
      */
     public function __construct(
         HttpClientInterface $client,
         string $tokenUrl,
         string $clientId,
         string $clientSecret,
+        array $scopes = []
     ) {
         $this->client = $client;
         $this->tokenUrl = $tokenUrl;
         $this->clientId = $clientId;
         $this->clientSecret = $clientSecret;
+        $this->scopes = $scopes;
     }
 
     /**
@@ -47,9 +55,15 @@ class ClientCredentialsGrantType implements GrantTypeInterface
      */
     public function getTokens(): Tokens
     {
+        $body = ['grant_type' => 'client_credentials'];
+
+        if ($this->scopes) {
+            $body['scope'] = $this->scopes;
+        }
+
         $response = $this->client->request('POST', $this->tokenUrl, [
             'headers' => ['Authorization' => sprintf('Basic %s', base64_encode("{$this->clientId}:{$this->clientSecret}"))],
-            'body' => http_build_query(['grant_type' => 'client_credentials']),
+            'body' => http_build_query($body),
         ]);
 
         return $this->extractTokens($response);
